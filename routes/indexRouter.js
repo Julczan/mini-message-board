cf2f81823d689dc9acddddf9dcb83b1f6633a46f
Julczan <julek111222h@gmail.com>
2026-05-29 20:21:52 +0200

    display the user messages with views

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,7 +1,23 @@
 const { Router } = require("express");
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => res.send("Index route"));
+indexRouter.get("/", (req, res) => 
+{
+    res.render("index", {messages: messages})
+});
 
 module.exports = indexRouter;
