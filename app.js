c5d5b37fe23c384dc87c1be7714d61678a05cb59
Julczan <julek111222h@gmail.com>
2026-05-30 14:51:34 +0200

    handle error middleware

--- app.js
+++ app.js
@@ -13,7 +13,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
 
-app.use("/message", messageRouter)
+app.use("/message", messageRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err);
+});
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
