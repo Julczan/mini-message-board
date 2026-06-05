39308f06a6521116a278d96240a7cb0bdc51d1ca
Julczan <julek111222h@gmail.com>
2026-06-05 14:13:50 +0200

    get All messages from db

--- controllers/getMessages.js
+++ controllers/getMessages.js
@@ -1,20 +1,10 @@
-const messages = [
-  {
-    id: crypto.randomUUID(),
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    id: crypto.randomUUID(),
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
+const { getAllMessages } = require("../db/queries");
 
-function getMessages(req, res, next){
-    res.render("index", {messages: messages})
+async function getMessages(req, res) {
+  const messages = await getAllMessages();
+  console.log(messages);
+
+  res.render("index", { messages: messages });
 }
 
-module.exports = {getMessages, messages};
+module.exports = { getMessages };
---
+++ db/queries.js
@@ -0,0 +1,18 @@
+const pool = require("./pool");
+
+async function getAllMessages() {
+  const { rows } = await pool.query("SELECT * FROM messages");
+  return rows;
+}
+
+async function insetMessage(message, username) {
+  await pool.query("INSERT INTO messages (message, username) VALUES ($1, $2)", [
+    message,
+    username,
+  ]);
+}
+
+module.exports = {
+  getAllMessages,
+  insetMessage,
+};
